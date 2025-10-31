91e67b7ab0919bf33cc07c176e8418ad7e6da5c6
Rohith-kumar18 <vtu28785@veltech.edu.in>
2025-10-31 16:48:25 +0530

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -1,24 +1,32 @@
-graph = {
-    '5': ['3', '7'],
-    '3': ['2', '4'],
-    '7': ['8'],
-    '2': [],
-    '4': ['8'],
-    '8': []
-}
+class Graph:
+    def _init_(self):
+        self.adj_list = {}
+        self.visited = set()
 
-def dfs(graph, start):
-    visited = []
-    stack = []
-    stack.append(start)
+    def add_edge(self, u, v):
+        if u not in self.adj_list:
+            self.adj_list[u] = [v]
+        else:
+            self.adj_list[u].append(v)
 
-    while stack:
-        node = stack.pop()
-        if node not in visited:
-            print(node, end=" ")
-            visited.append(node)
-            for neighbour in reversed(graph[node]):
-                if neighbour not in visited:
-                    stack.append(neighbour)
+        if v not in self.adj_list:
+            self.adj_list[v] = [u]
+        else:
+            self.adj_list[v].append(u)
 
-dfs(graph, '5')
+    def dfs(self, node):
+        if node not in self.visited:
+            self.visited.add(node)
+            print(node, end=' ')
+            for neighbour in self.adj_list[node]:
+                self.dfs(neighbour)
+
+g = Graph()
+g.add_edge('A', 'B')
+g.add_edge('A', 'C')
+g.add_edge('B', 'E')
+g.add_edge('A', 'D')
+g.add_edge('C', 'E')
+
+print("DFS Traversal:")
+g.dfs('A')
